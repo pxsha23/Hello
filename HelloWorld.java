f68a6998b8204c7c956899e92e6745c4752d85eb
Piyusha <pxsha23@gmail.com>
2026-02-06 15:37:48 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hellooo, World!");
-        System.out.println("Test build trigger");
+        System.out.println("Jenkins Test build trigger");
     }
 }
